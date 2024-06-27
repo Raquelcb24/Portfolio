17358f113e71dfd4902377e1b600d0d40fce03f6
Raquel Castro <raquelcastrobermudo@gmail.com>
2024-06-27 18:44:21 +0200

    pages

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -7,7 +7,7 @@ function Aboutme() {
             <img className="main__img" src="../public/images/photo-noBackground.png" alt="Foto Raquel Castro"/>
             <div className="main__content">
                 <h2 className="main__content--title">About me</h2>
-                <p className="main__content--text"> I love challenges, which motivated me to reinvent myself, change my professional field and became a web developer, where I have found a great motivation and enthusiasm to continue learning. I have knowledge in HTML, CSS, JavaScript and modern libraries such as React, and I am able to work effectively both independently and in a teamwork. Highly organized and solution-oriented, I am committed to continuous learning and improving my technical skills.</p>
+                <p className="main__content--text">Coming from a social background, I bring strong communication, conflict resolution, and teamwork skills. I love challenges, which motivated me to reinvent myself, change my professional field and became a web developer, where I have found a great motivation and enthusiasm to continue learning. I have knowledge in HTML, CSS, JavaScript and modern libraries such as React, and I am able to work effectively both independently and in a teamwork. Highly organized and solution-oriented, I am committed to continuous learning and improving my technical skills.</p>
                 
             </div>
     
