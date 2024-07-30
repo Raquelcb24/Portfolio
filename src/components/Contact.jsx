d4482f7ecee4641831ca31db9e7507f0a5252c34
Raquel Castro <raquelcastrobermudo@gmail.com>
2024-07-30 15:22:23 +0200

    hover

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,17 +9,10 @@ function Contact() {
     <>
     <h2 id="contact" className="contact__title">Contact me</h2>
     <ul className="contact__container">
-        
-        <li> <a href={`mailto:${email}`} target="_blank">Email <FontAwesomeIcon icon={faEnvelope} /></a> </li>
-        
-        <li> <a href={`tel:${phone}`} target="_blank">Phone <FontAwesomeIcon icon={faMobileScreen} /></a> </li>
-       
-        <li><a href="https://github.com/Raquelcb24" target="_blank">Github <FontAwesomeIcon icon={faGithub} /> </a></li>
-        <li><a href="https://www.linkedin.com/in/raquelcb24/" target="_blank">Linkedin <FontAwesomeIcon icon={faLinkedin} /> </a></li>
-            
-            
-      
-        
+        <li className='ico'><a href={`mailto:${email}`} target="_blank">Email <FontAwesomeIcon icon={faEnvelope} /></a> </li>
+        <li className='ico'><a href={`tel:${phone}`} target="_blank">Phone <FontAwesomeIcon icon={faMobileScreen} /></a> </li>
+        <li className='ico'><a href="https://github.com/Raquelcb24" target="_blank">Github <FontAwesomeIcon icon={faGithub} /> </a></li>
+        <li className='ico'><a href="https://www.linkedin.com/in/raquelcb24/" target="_blank">Linkedin <FontAwesomeIcon icon={faLinkedin} /> </a></li>
     </ul>
     
     </>
